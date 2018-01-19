651f6056768eebb25708ffc4133f00f01e0da125
Sandro Mani <manisandro@gmail.com>
2018-01-19 18:31:59 +0100

    Move proxy to separate service

--- permalink.py
+++ permalink.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, jsonify, Response, stream_with_context
 from flask_cors import CORS
-import requests
-import os
 import hashlib
 import random
 import json
@@ -15,35 +13,11 @@ CORS(app)
 
 permalinks = {}
 
-@app.route("/proxy", methods=['GET','POST','PUT','DELETE'])
-# proxy?url=<url>&filename=<filename>
-# url: the url to proxy
-# filename: optional, if set it sets a content-disposition header with the specified filename
-def proxy():
-    url = request.args.get('url')
-    filename = request.args.get('filename')
-    if request.method == 'POST':
-        headers={'content-type': request.headers['content-type']}
-        req = requests.post(url, stream=True, timeout=30, data=request.get_data(), headers=headers)
-    elif request.method == 'PUT':
-        headers={'content-type': request.headers['content-type']}
-        req = requests.put(url, stream=True, timeout=30, data=request.get_data(), headers=headers)
-    elif request.method == 'DELETE':
-        req = requests.delete(url, stream=True, timeout=10)
-    elif request.method == 'GET':
-        req = requests.get(url, stream=True, timeout=10)
-    else:
-        raise "Invalid operation"
-    response = Response(stream_with_context(req.iter_content(chunk_size=1024)), status=req.status_code)
-    if filename:
-        response.headers['content-disposition'] = 'attachment; filename=' + filename
-    response.headers['content-type'] = req.headers['content-type']
-    return response
-
 @app.route("/createpermalink", methods=['GET','POST'])
-# createpermalink?url=<url>
+# /createpermalink?url=<url>
 # url: the url for which to generate a permalink
-# output: a json document {permalink: <permalink_url>}
+# payload: a json document with additional state information
+# output: a json document: `{permalink: <permalink_url>}`
 def createpermalink():
     url = request.args['url']
     parts = urlparse(url)
@@ -66,9 +40,11 @@ def createpermalink():
     return jsonify(**result)
 
 @app.route("/resolvepermalink")
-# resolvepermalink?key=<key>
+# /resolvepermalink?key=<key>
 # key: the key query parameter of the permalink url
-# output: a json document containing all query parameters which were encoded in the permalink key
+# output: a json document: `{query: <query_parameters>, state: <state>}`
+#   - `query_parameters`: all query parameters of the URL passed to `createpermalink`
+#   - `state`: the state data passed as POST payload to `createpermalink`
 def resolvepermalink():
     key = request.args['key']
     data = {}
@@ -78,4 +54,4 @@ def resolvepermalink():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5001)
---
+++ proxy.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, Response, stream_with_context
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=['GET','POST','PUT','DELETE'])
+# /?url=<url>&filename=<filename>
+# url: the url to proxy
+# filename: optional, if set it sets a content-disposition header with the specified filename
+def proxy():
+    url = request.args.get('url')
+    filename = request.args.get('filename')
+    if request.method == 'POST':
+        headers={'content-type': request.headers['content-type']}
+        req = requests.post(url, stream=True, timeout=30, data=request.get_data(), headers=headers)
+    elif request.method == 'PUT':
+        headers={'content-type': request.headers['content-type']}
+        req = requests.put(url, stream=True, timeout=30, data=request.get_data(), headers=headers)
+    elif request.method == 'DELETE':
+        req = requests.delete(url, stream=True, timeout=10)
+    elif request.method == 'GET':
+        req = requests.get(url, stream=True, timeout=10)
+    else:
+        raise "Invalid operation"
+    response = Response(stream_with_context(req.iter_content(chunk_size=1024)), status=req.status_code)
+    if filename:
+        response.headers['content-disposition'] = 'attachment; filename=' + filename
+    response.headers['content-type'] = req.headers['content-type']
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
